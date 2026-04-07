8ca0c52e63a8520ee7862a73b4c1dccacf94e118
re-nanashi <jfabro3124@gmail.com>
2026-04-07 16:12:23 +0800

    feat: init

---
+++ backend/src/main/java/com/mori/api/MoriApiApplication.java
@@ -0,0 +1,12 @@
+package com.mori.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoriApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoriApiApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/mori/api/MoriApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mori.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoriApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
