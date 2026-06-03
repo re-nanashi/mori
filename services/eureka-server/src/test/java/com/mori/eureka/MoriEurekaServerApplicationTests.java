8b02e5b1ead5c24d0a488319cf00efe975a5c036
re-nanashi <jfabro3124@gmail.com>
2026-06-03 09:26:00 +0800

    feat(eureka): set up eureka server

---
+++ services/eureka-server/src/test/java/com/mori/eureka/MoriEurekaServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mori.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoriEurekaServerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
