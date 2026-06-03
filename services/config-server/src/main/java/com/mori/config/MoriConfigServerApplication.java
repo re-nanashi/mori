2cf929ca12df5ac9bd09002c281fc5b1decf653c
re-nanashi <jfabro3124@gmail.com>
2026-06-03 11:21:14 +0800

    feat(config): initialize config server

---
+++ services/config-server/src/main/java/com/mori/config/MoriConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mori.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MoriConfigServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MoriConfigServerApplication.class, args);
+	}
+}
